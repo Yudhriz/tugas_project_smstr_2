34b841cf0cd614fe40c03cf9b3b6d9562e3b3859
Yudhriz <riskyyudha678@gmail.com>
2023-05-06 15:46:11 +0700

    update

--- admin/table_pesanan/view_pesanan.php
+++ admin/table_pesanan/view_pesanan.php
@@ -6,7 +6,7 @@ $_id = $_GET['id'];
 // select * from produk where id = $_id;
 //$sql = "SELECT a.*,b.nama as jenis FROM produk a
 //INNER JOIN jenis_produk b ON a.jenis_produk_id=b.id WHERE a.id=?";
-$sql = "SELECT * FROM kategori_produk WHERE id=?";
+$sql = "SELECT * FROM pesanan WHERE id=?";
 $st = $dbh->prepare($sql);
 $st->execute([$_id]);
 $row = $st->fetch();
@@ -24,7 +24,7 @@ $row = $st->fetch();
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="index.php">Home</a></li>
                     <li class="breadcrumb-item active">Master Data</li>
-                    <li class="breadcrumb-item active">Kategori Produk</li>
+                    <li class="breadcrumb-item active">Pesanan</li>
                 </ol>
             </div><!-- /.col -->
         </div><!-- /.row -->
@@ -42,8 +42,36 @@ $row = $st->fetch();
                     <td><?= $row['id'] ?></td>
                 </tr>
                 <tr>
-                    <td>Nama Kategori Produk</td>
-                    <td><?= $row['nama'] ?></td>
+                    <td>Tanggal</td>
+                    <td><?= $row['tanggal'] ?></td>
+                </tr>
+                <tr>
+                    <td>Nama Pemesan</td>
+                    <td><?= $row['nama_pemesan'] ?></td>
+                </tr>
+                <tr>
+                    <td>Alamat Pemesan</td>
+                    <td><?= $row['alamat_pemesan'] ?></td>
+                </tr>
+                <tr>
+                    <td>No HP</td>
+                    <td><?= $row['no_hp'] ?></td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td><?= $row['email'] ?></td>
+                </tr>
+                <tr>
+                    <td>Jumlah Pesanan</td>
+                    <td><?= $row['jumlah_pesanan'] ?></td>
+                </tr>
+                <tr>
+                    <td>Deskripsi</td>
+                    <td><?= $row['deskripsi'] ?></td>
+                </tr>
+                <tr>
+                    <td>Produk id</td>
+                    <td><?= $row['produk_id'] ?></td>
                 </tr>
             </tbody>
         </table>
